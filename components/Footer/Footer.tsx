23f7da2edbce5c62ee5e08cd6534ef7dd4f101c3
Niel Berongoy <niel_berongoy@yahoo.com>
2021-07-21 19:07:37 +0800

    feat common added footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from '@chakra-ui/react';
+import React, { FC } from 'react';
+
+const Footer: FC = () => (
+  <Box p="10" backgroundColor="white">
+    <Text>© 2020 HOV Onboarding. All rights reserved.</Text>
+  </Box>
+);
+
+export default Footer;
