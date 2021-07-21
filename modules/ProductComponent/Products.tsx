7a1b106d5b369031575484c9081ef7226a5905ca
Niel Berongoy <niel_berongoy@yahoo.com>
2021-07-21 15:34:45 +0800

    feat common loop product card

--- modules/ProductComponent/Products.tsx
+++ modules/ProductComponent/Products.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Heading } from '@chakra-ui/react';
+import { Box, Divider, Grid, Heading } from '@chakra-ui/react';
 import React, { FC } from 'react';
 import ProductCard from './ProductCard';
 
@@ -9,10 +9,16 @@ const Products: FC = () => (
     </Box>
 
     <Divider />
-
-    <Box mt="10">
-      <ProductCard />
-    </Box>
+    <Grid templateColumns="repeat(5, 1fr)" gap={10}>
+      {Array(20)
+        .fill('')
+        .map(() => (
+          // eslint-disable-next-line react/jsx-key
+          <Box mt="10">
+            <ProductCard />
+          </Box>
+        ))}
+    </Grid>
   </Box>
 );
 
