4f16e62ccd5486da623d639feda70c2f2575f28e
Niel Berongoy <niel_berongoy@yahoo.com>
2021-07-26 13:08:38 +0800

    feat common added a route active link in navigation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,26 +1,36 @@
 import { Avatar, Box, Button, Flex, Spacer, Wrap, WrapItem } from '@chakra-ui/react';
 import styles from '@styles/Navigation.module.css';
+import { navigationMenu } from '@utils/constants/navigation';
 import { isLoggedIn } from '@utils/helper/auth';
 import NextImage from 'next/image';
 import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import React, { FC, useEffect, useState } from 'react';
 import { BiBell } from 'react-icons/bi';
 
 const Navigation: FC = () => {
   const [hasLoggedIn, setHasLoggedIn] = useState<boolean>(false);
+  const router = useRouter();
 
   useEffect(() => {
     setHasLoggedIn(isLoggedIn());
   }, []);
 
   return (
-    <Box p="2" backgroundColor="white" className={styles.navigationContainer}>
+    <Box backgroundColor="white" className={styles.navigationContainer}>
       <Flex>
-        <Box ml="24" p="2">
+        <Box ml="24" p="4">
           <NextLink href="/">
             <NextImage src="/Logo.png" width={143} height={32} className={styles.companyLogo} />
           </NextLink>
         </Box>
+        {navigationMenu.map((item, index) => {
+          return (
+            <Box key={index} ml="5" mt="5" className={router.pathname === item.path ? styles.activeLink : ''}>
+              <NextLink href={item.path}>{item.title}</NextLink>
+            </Box>
+          );
+        })}
         <Spacer />
         <Box mr="24" p="2">
           {!hasLoggedIn ? (
